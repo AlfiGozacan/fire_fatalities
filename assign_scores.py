015366ae4c2f8183b99f73f0dc546d40437a1808
Alfi Gozacan <alfigozacan@gmail.com>
2022-02-10 12:05:54 +0000

    Add files via upload

--- assign_scores.py
+++ assign_scores.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 ### Load and create local data --------------
 print("Loading and creating local data...")
 
-file_path = "C:\\path_to_data\\"
+file_path = "C:\\Users\\agozacan\\OneDrive - Humberside Fire and Rescue Service\\Fire Fatality Profiling\\Input and Output\\"
 
 mosaic_means = pd.read_csv(file_path+"mosaic_means.csv")
 
@@ -28,9 +28,10 @@ multipliers = pd.DataFrame({
                    "Is a smoker and female",
                    "Has restricted mobility",
                    "Regularly drinks alcohol once or more per day",
-                   "Is living in social rented housing"],
-    "Multiplier" : [1.00, 1.78, 2.00, 2.08, 2.37, 2.63, 4.52, 6.68, 1.10, 1.10, 3.89],
-    "Mosaic_Index" : ["--", 31, "--", "--", "--", "--", [1033, 0], [1033, 0], 1156, 1049, 102]})
+                   "Is living in social rented housing",
+                   "Lives outside of 8 minute response zone"],
+    "Multiplier" : [1.00, 1.78, 2.00, 2.08, 2.37, 2.63, 4.52, 6.68, 1.10, 1.10, 3.89, 1.10],
+    "Mosaic_Index" : ["--", 31, "--", "--", "--", "--", [1033, 0], [1033, 0], 1278, 1049, 102, "--"]})
 
 ### Connect to SQL Server -------------------
 print("Connecting to SQL Server (HQCFRMISSQL)...")
@@ -89,7 +90,7 @@ for i in range(-66, 0):
 
         mosaic_scores.loc[i+66, "Smoker"] = 1
 
-    if mosaic_means.iloc[1156, i] > mosaic_means.iloc[1156, -82]:
+    if mosaic_means.iloc[1278, i] > mosaic_means.iloc[1278, -82]:
 
         mosaic_scores.loc[i+66, "Restricted_Mobility"] = 1
 
@@ -108,6 +109,10 @@ df = df.merge(right=mosaic_scores, left_on="Type_Desc", right_on="Mosaic_Type", 
 for i in tqdm(range(len(df))):
 
     score = multipliers.iloc[0, 1]
+
+    if df.loc[i, "Response"] == "Outside":
+
+        score = score * multipliers.iloc[11, 1]
 
     if df.loc[i, "Restricted_Mobility"] == 1:
 
--- scoring_evaluation.R
+++ scoring_evaluation.R
@@ -4,10 +4,13 @@ library(tidyverse)
 library(RODBC)
 library(leaflet)
 library(rgdal)
+library(plotly)
 
 # Load data -------------------
 
-scores <- read.csv("C:/Users/agozacan/OneDrive - Humberside Fire and Rescue Service/Fire Fatality Profiling/Data/final_scores_with_othercircs.csv")
+scores <- read.csv("C:/Users/agozacan/OneDrive - Humberside Fire and Rescue Service/Fire Fatality Profiling/Input and Output/output.csv")
+
+scores = scores[-which(scores$Mosaic_Type == ""),]
 
 ranks <- data.frame("Final_Score" = unique(scores$Final_Score))
 
@@ -30,21 +33,63 @@ incidents <- sqlQuery(dbhandle,
                       where NUMBER_OF_CASUALTIES > 0
                       or NUMBER_OF_FATALITIES > 0")
 
+dbhandle2 <- odbcDriverConnect("driver={SQL Server};
+                              server=HQCFRMISSQL;
+                              database=CFRMIS_HUMBS;
+                              trusted_connection=true")
+
+old_scores <- sqlQuery(dbhandle2,
+                       "select UPRN, Easting, Northing, Type_Desc, Total_Prio, Final_Prio
+                       from MosaicCurrent
+                       ")
+
+old_scores = old_scores[-which(old_scores$Type_Desc == "U-99"),]
+
+old_ranks <- data.frame("Total_Prio" = unique(old_scores$Total_Prio))
+
+for (i in 1:length(old_ranks$Total_Prio)) {
+  
+  old_ranks$rank[i] = which(sort(old_ranks$Total_Prio) == old_ranks$Total_Prio[i])
+  
+}
+remove(i)
+
+# The Mosaic types recorded in MosaicCurrent are from Mosaic UK 7, whereas INCIDENTS uses Mosaic Public Sector -----------
+
+old_scores %>%
+  right_join(incidents %>%
+              mutate(UPRN = as.numeric(UPRN)) %>%
+              select(UPRN, CALLDATE, NUMBER_OF_CASUALTIES, NUMBER_OF_FATALITIES, RESCUES),
+            by="UPRN")
+
 # Make plots -----------------
 
-# Score distribution
+# Count number of dwellings by priority score
 scores %>%
+  count(Final_Score) %>%
+  arrange(Final_Score)
+
+old_scores %>%
+  count(Final_Prio) %>%
+  arrange(Final_Prio)
+
+# Score distribution
+p <- scores %>%
   ggplot(aes(Final_Score)) +
   geom_density()
 
+ggplotly(p)
+
 # Score rank distribution (higher score equates to lower rank)
-scores %>%
+p <- scores %>%
   left_join(ranks, by="Final_Score") %>%
   ggplot(aes(rank)) +
-  geom_density()
+  geom_histogram(bins=76)
 
-# Proportion of incidents with casualties / fatalities by score rank (76-tile)
-scores %>%
+ggplotly(p)
+
+# Proportion of incidents with casualties / fatalities by score rank
+p <- scores %>%
   left_join(ranks, by="Final_Score") %>%
   left_join(incidents %>%
               mutate(UPRN = as.numeric(UPRN)) %>%
@@ -62,9 +107,11 @@ scores %>%
   ggplot(aes(rank, propn_casfat)) +
   geom_col()
 
+ggplotly(p)
+
 # Proportion of incidents with casualties / fatalities by score ntile
-scores %>%
-  mutate(ntile = ntile(Final_Score, 10)) %>%
+p <- scores %>%
+  mutate(ntile = ntile(Final_Score, 9)) %>%
   left_join(incidents %>%
               mutate(UPRN = as.numeric(UPRN)) %>%
               select(UPRN, CALLDATE, NUMBER_OF_CASUALTIES, NUMBER_OF_FATALITIES),
@@ -74,43 +121,153 @@ scores %>%
   summarise(total_casualties = sum(NUMBER_OF_CASUALTIES),
             total_fatalities = sum(NUMBER_OF_FATALITIES)) %>%
   mutate(total_casfat = total_casualties + total_fatalities) %>%
-  ggplot(aes(ntile, total_casfat)) +
-  geom_col()
+  ggplot(aes(as.factor(ntile), total_casfat)) +
+  geom_col(fill="royalblue") +
+  ggtitle("Number of incidents involving a casualty or a fatality in Humberside by 9-tile (Proposed Model)") +
+  xlab("9-tile (higher score equates to higher risk)") +
+  ylab("Number of Casualty or Fatality Incidents")
 
-# Proportion of incidents with casualties / fatalities by Mosaic type
-scores %>%
+p <- old_scores %>%
+  left_join(old_ranks, by="Total_Prio") %>%
+  left_join(incidents %>%
+              mutate(UPRN = as.numeric(UPRN)) %>%
+              select(UPRN, CALLDATE, NUMBER_OF_CASUALTIES, NUMBER_OF_FATALITIES),
+            by="UPRN") %>%
+  replace_na(list(NUMBER_OF_CASUALTIES = 0, NUMBER_OF_FATALITIES = 0)) %>%
+  group_by(Final_Prio, rank) %>%
+  summarise(total_casualties = sum(NUMBER_OF_CASUALTIES),
+            total_fatalities = sum(NUMBER_OF_FATALITIES),
+            frequency = n()) %>%
+  mutate(total_casfat = total_casualties + total_fatalities) %>%
+  mutate(propn_casfat = total_casfat / frequency) %>%
+  ggplot(aes(reorder(Final_Prio, rank), propn_casfat)) +
+  geom_col(fill="tomato") +
+  ggtitle("Proportion of households where a casualty or fatality has been recorded by priority score (Current Model)") +
+  xlab("Priority Score") +
+  ylab("Proportion of Households")
+
+ggplotly(p)
+
+# Proportion and number of incidents with casualties / fatalities by Mosaic type
+p <- scores %>%
   left_join(incidents %>%
               mutate(UPRN = as.numeric(UPRN)) %>%
               select(UPRN, CALLDATE, NUMBER_OF_CASUALTIES, NUMBER_OF_FATALITIES),
             by="UPRN") %>%
   replace_na(list(NUMBER_OF_CASUALTIES = 0, NUMBER_OF_FATALITIES = 0)) %>%
   group_by(Mosaic_Type) %>%
-  summarise(total_casualties = sum(NUMBER_OF_CASUALTIES),
-            total_fatalities = sum(NUMBER_OF_FATALITIES)) %>%
-  mutate(total_casfat = total_casualties + total_fatalities) %>%
-  ggplot(aes(Mosaic_Type, total_casfat)) +
-  geom_col()
+  summarise(prop = (sum(NUMBER_OF_CASUALTIES) + sum(NUMBER_OF_FATALITIES)) / n()) %>%
+  ggplot(aes(reorder(Mosaic_Type, -prop), prop)) +
+  geom_col() +
+  ggtitle("Proportion of households where a casualty or fatality has been recorded by Mosaic type") +
+  xlab("Mosaic Type") +
+  ylab("Proportion of Households") +
+  theme(axis.text.x = element_text(angle = 90))
+
+p <- scores %>%
+  left_join(incidents %>%
+              mutate(UPRN = as.numeric(UPRN)) %>%
+              select(UPRN, CALLDATE, NUMBER_OF_CASUALTIES, NUMBER_OF_FATALITIES),
+            by="UPRN") %>%
+  replace_na(list(NUMBER_OF_CASUALTIES = 0, NUMBER_OF_FATALITIES = 0)) %>%
+  group_by(Mosaic_Type) %>%
+  summarise(freq = sum(NUMBER_OF_CASUALTIES) + sum(NUMBER_OF_FATALITIES)) %>%
+  ggplot(aes(reorder(Mosaic_Type, -freq), freq)) +
+  geom_col() +
+  ggtitle("Number of households where a casualty or fatality has been recorded by Mosaic type") +
+  xlab("Mosaic Type") +
+  ylab("Number of Households") +
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplotly(p)
 
 # Average final score by Mosaic type
-scores %>%
+p <- scores %>%
   group_by(Mosaic_Type) %>%
   summarise(avg_score = mean(Final_Score)) %>%
-  ggplot(aes(Mosaic_Type, avg_score)) +
+  ggplot(aes(reorder(Mosaic_Type, -avg_score), avg_score)) +
+  geom_col(fill="royalblue") +
+  ggtitle("Average Risk Score by Mosaic Type (Proposed Model)") +
+  xlab("Mosaic Type") +
+  ylab("Average Score") +
+  theme(axis.text.x = element_text(angle = 90))
+
+p <- old_scores %>%
+  group_by(Type_Desc) %>%
+  summarise(avg_score = mean(Total_Prio)) %>%
+  ggplot(aes(reorder(Type_Desc, -avg_score), avg_score)) +
+  geom_col(fill="tomato") +
+  ggtitle("Average Risk Score by Mosaic Type (Current Model)") +
+  xlab("Mosaic Type") +
+  ylab("Average Score") +
+  theme(axis.text.x = element_text(angle = 90))
+
+p <- old_scores %>%
+  group_by(Type_Desc) %>%
+  summarise(avg_score = median(Total_Prio)) %>%
+  ggplot(aes(reorder(Type_Desc, -avg_score), avg_score)) +
   geom_col()
 
-# Number of high-priority dwellings by Mosaic type
-scores %>%
-  filter(Final_Score > 100) %>%
+ggplotly(p)
+
+# Number of dwellings by Mosaic type
+p <- scores %>%
   group_by(Mosaic_Type) %>%
-  summarise(count = n()) %>%
-  ggplot(aes(Mosaic_Type, count)) +
+  summarise(total = n()) %>%
+  ggplot(aes(reorder(Mosaic_Type, -total), total)) +
   geom_col()
+
+ggplotly(p)
+
+# Number of high-priority dwellings by Mosaic type
+p <- scores %>%
+  mutate(high_priority = (Final_Score > 130)) %>%
+  group_by(Mosaic_Type) %>%
+  summarise(total = sum(high_priority)) %>%
+  filter(total > 0) %>%
+  ggplot(aes(reorder(Mosaic_Type, -total), total)) +
+  geom_col()
+
+ggplotly(p)
+
+# Proportion of high-priority dwellings by Mosaic type
+p <- scores %>%
+  mutate(high_priority = (Final_Score > 130)) %>%
+  group_by(Mosaic_Type) %>%
+  summarise(total = sum(high_priority),
+            prop = sum(high_priority) / n()) %>%
+  filter(prop > 0) %>%
+  ggplot(aes(reorder(Mosaic_Type, -prop), prop)) +
+  geom_col(fill="royalblue") +
+  ggtitle("Proportion of high-priority dwellings by Mosaic type (with total number of high-piority dwellings)") +
+  xlab("Mosaic Type") +
+  ylab("Proportion") +
+  geom_label(aes(label=total), nudge_y = -.01, fill="white")
+
+p <- old_scores %>%
+  mutate(high_priority = (Total_Prio == 22)) %>%
+  group_by(Type_Desc) %>%
+  summarise(total = sum(high_priority),
+            prop = sum(high_priority) / n()) %>%
+  filter(prop > 0) %>%
+  ggplot(aes(reorder(Type_Desc, -prop), prop)) +
+  geom_col(fill="tomato") +
+  ggtitle("Proportion of high-priority dwellings by Mosaic type (with total number of high-piority dwellings)") +
+  xlab("Mosaic Type") +
+  ylab("Proportion") +
+  geom_label(aes(label=total), nudge_y = -.01, fill="white")
+
+ggplotly(p)
 
 # Make map of high-priority areas -----------------
 
-# Convert coords
+# Convert new coords
 coords.EPSG.27700 = SpatialPoints(cbind(scores[scores$Final_Score > 130,]$Easting, scores[scores$Final_Score > 130,]$Northing), proj4string=CRS("+init=epsg:27700"))                                                                                      # Easting / Northing
 coords.EPSG.4326 <- spTransform(coords.EPSG.27700, CRS("+init=epsg:4326"))                                     # Long / Lat
+
+# Convert old coords
+coords.EPSG.27700 = SpatialPoints(cbind(old_scores[old_scores$Total_Prio == 22,]$Easting, old_scores[old_scores$Total_Prio == 22,]$Northing), proj4string=CRS("+init=epsg:27700"))                                                                                      # Easting / Northing
+coords.EPSG.4326 <- spTransform(coords.EPSG.27700, CRS("+init=epsg:4326"))     
 
 leaflet() %>%
   addTiles() %>%
