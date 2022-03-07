9f4fccef2b43fffa0996e947067fcffa4f3f26bf
Alfi Gozacan <alfigozacan@gmail.com>
2022-03-07 12:56:07 +0000

    Add files via upload

--- assign_scores.py
+++ assign_scores.py
@@ -1,7 +1,6 @@
 ### Load libraries --------------------------
 print("Loading libraries...")
 
-import pyodbc
 import pandas as pd
 import numpy as np
 import datetime
@@ -15,6 +14,10 @@ print("Loading and creating local data...")
 
 file_path = "C:\\Users\\agozacan\\OneDrive - Humberside Fire and Rescue Service\\Fire Fatality Profiling\\Input and Output\\"
 
+dwellings = pd.read_csv(file_path+"dwellings.csv", encoding="latin-1")
+
+response = pd.read_csv(file_path+"response.csv")
+
 mosaic_means = pd.read_csv(file_path+"mosaic_means.csv")
 
 exeter = pd.read_csv(file_path+"exeter_data.csv")
@@ -22,14 +25,12 @@ exeter = pd.read_csv(file_path+"exeter_data.csv")
 initial_length = len(exeter)
 
 exeter.drop(exeter.index[exeter["Address_Line_1"].str.contains("care home", case=False, regex=True).replace(np.nan, False)], axis=0, inplace=True)
-
 exeter.drop(exeter.index[exeter["Address_Line_1"].str.contains("residential", case=False, regex=True).replace(np.nan, False)], axis=0, inplace=True)
+exeter.reset_index(drop=True, inplace=True)
 
 final_length = len(exeter)
 
 print(f"{initial_length - final_length} entries in the Exeter dataset have been dropped due to them referencing care homes.")
-
-exeter.reset_index(drop=True, inplace=True)
 
 multipliers = pd.DataFrame({
     "Attribute" : ["Base score",
@@ -47,40 +48,75 @@ multipliers = pd.DataFrame({
     "Multiplier" : [1.00, 1.78, 2.00, 2.08, 2.37, 2.63, 4.52, 6.68, 1.10, 1.10, 3.89, 1.10],
     "Mosaic_Index" : ["--", 31, "--", "--", "--", "--", [1033, 0], [1033, 0], 1278, 1049, 102, "--"]})
 
-### Connect to SQL Server -------------------
-print("Connecting to SQL Server (HQCFRMISSQL)...")
-
-server = "HQCFRMISSQL"
-database = "CFRMIS_HUMBS"
-
-cnxn = pyodbc.connect("DRIVER={SQL Server};SERVER="+server+";DATABASE="+database)
-
-### Load server data
-print("Loading server data...")
-
-query = '''
-select *
-from MosaicCurrent
-'''
-
-dwellings = pd.read_sql(query, cnxn)
-
 ### Clean data
 print("Cleaning data...")
 
-dwellings.drop(dwellings.index[dwellings["Type_Desc"] == "U-99"], axis=0, inplace=True)
+dwelling_cols = ["Addressbase UPRN",
+                 "Sub Building Name",
+                 "Building Name",
+                 "Street Number",
+                 "Dependent Street",
+                 "Street",
+                 "Double Dependent Locality",
+                 "Dependent Locality",
+                 "Town",
+                 "Postcode",
+                 "Addressbase Easting",
+                 "Addressbase Northing",
+                 "(H) Mosaic UK 7 Type Label",
+                 "(PC) Output Area (OA)",
+                 "(PC) Super Output Areas - Lower Layer",
+                 "(PC) Super Output Areas - Middle Layer",
+                 "(PC) Electoral Wards",
+                 "(PC) Local Authority Districts and Unitary Authorities Code",
+                 "(PC) Counties and Unitary Authorities Code"]
+
+dwellings = dwellings[dwelling_cols]
+
+dwellings.rename(columns = {
+    "Addressbase UPRN" : "UPRN",
+    "Addressbase Easting" : "Easting",
+    "Addressbase Northing" : "Northing",
+    "(H) Mosaic UK 7 Type Label" : "Type_Desc",
+    "(PC) Output Area (OA)" : "OA",
+    "(PC) Super Output Areas - Lower Layer" : "LSOA",
+    "(PC) Super Output Areas - Middle Layer" : "MSOA",
+    "(PC) Electoral Wards" : "Ward",
+    "(PC) Local Authority Districts and Unitary Authorities Code" : "Local Authority Code",
+    "(PC) Counties and Unitary Authorities Code" : "Counties Code"
+}, inplace=True)
+
+dwellings["Type_Desc"] = [x[:3] for x in dwellings["Type_Desc"]]
+
+initial_length = len(dwellings)
+
+dwellings.drop(dwellings.index[dwellings["UPRN"].isnull()], axis=0, inplace=True)
+dwellings.drop_duplicates(subset="UPRN", keep="first", inplace=True)
+dwellings.drop(dwellings.index[dwellings["Type_Desc"] == "U99"], axis=0, inplace=True)
 dwellings.reset_index(drop=True, inplace=True)
 
+final_length = len(dwellings)
+
+print(f"{initial_length - final_length} dwellings have been removed due to them having no UPRN, a duplicated UPRN, or no Mosaic type assigned.")
+
+response.drop("OID_", axis=1, inplace=True)
+response.rename(columns={
+    "InOut" : "Response"
+}, inplace=True)
+
+dwellings = dwellings.merge(right=response, left_on="UPRN", right_on="UPRN", how="left")
+
+dwellings.replace(np.nan, "", inplace=True)
 exeter.replace(np.nan, "", inplace=True)
 exeter["UPRN"].replace("", 0, inplace=True)
 
 dwellings["UPRN"] = [int(x) for x in dwellings["UPRN"]]
 exeter["UPRN"] = [int(x) for x in exeter["UPRN"]]
 
-bad_indices = exeter.index[~exeter["UPRN"].isin(dwellings["UPRN"])]
-
 dwellings["Postcode"].replace(" ", "", regex=True, inplace=True)
 exeter["Postcode"].replace(" ", "", regex=True, inplace=True)
+
+bad_indices = exeter.index[~exeter["UPRN"].isin(dwellings["UPRN"])]
 
 address_strings = []
 
@@ -130,13 +166,12 @@ exeter.loc[bad_indices, "is_Matched"] = 1
 
 exeter.loc[bad_indices, "Match_Score"] = final_fuzz_ratios
 
-remove_indices = exeter.index[exeter["Match_Score"] < 75]
+remove_indices = exeter.index[exeter["Match_Score"] < 95]
 
 exeter.drop(remove_indices, axis=0, inplace=True)
-
-print(f"{len(remove_indices)} entries in the Exeter dataset have been dropped due to their UPRNs not having a match.")
-
 exeter.reset_index(drop=True, inplace=True)
+
+print(f"{len(remove_indices)} entries in the Exeter dataset have been dropped due to their UPRNs not having a strong enough match.")
 
 exeter.rename(columns={"Postcode" : "Postcode_2"}, inplace=True)
 
@@ -145,8 +180,6 @@ year = now.year
 exeter["Age"] = [year - x for x in exeter["Year_Of_Birth"]]
 
 df = dwellings.merge(right=exeter, on="UPRN", how="left")
-
-df["Type_Desc"].replace("-", "", regex=True, inplace=True)
 
 ### Assign scores
 print("Assigning scores to all households...")
@@ -189,7 +222,7 @@ for i in tqdm(range(len(df))):
 
     score = multipliers.iloc[0, 1]
 
-    if df.loc[i, "Response"] == "Outside":
+    if df.loc[i, "Response"] == "Out":
 
         score = score * multipliers.iloc[11, 1]
 
@@ -256,31 +289,14 @@ for i in tqdm(range(len(df))):
 ### Tidy up columns and split into quantiles
 print("Tidying output and splitting into quantiles...")
 
-df.drop(["Group_Desc",
-         "Type_Desc",
-         "Exeter",
-         "Total_Mosa",
-         "Total_Prio",
-         "Final_Prio",
+df.drop(["Type_Desc",
          "Address_Line_5",
          "firearea",
          "firename",
          "Frailty_Score",
          "Frailty_Group"], axis=1, inplace=True)
 
-df.rename(columns = {
-    "SubBuildin" : "Sub_Building",
-    "BuildingNa" : "Building_Name",
-    "StreetNumb" : "Street_Number",
-    "DependentS" : "Dependent_Street",
-    "DoubleDepe" : "Double_Dependent_Locality",
-    "DependentL" : "Dependent_Locality",
-    "NAME" : "Local_Authority",
-    "STNNAME" : "Station_Number",
-    "NAME_1" : "Ward"
-}, inplace=True)
-
-df.insert(df.columns.get_loc("Ward")+1, "is_Exeter", [int(x) for x in ~df["Gender"].isnull()])
+df.insert(df.columns.get_loc("Gender"), "is_Exeter", [int(x) for x in ~df["Gender"].isnull()])
 
 initial_length = len(df)
 
@@ -291,7 +307,7 @@ final_length = len(df)
 num_exeter = len(df[df["is_Exeter"] == 1])
 
 print(f"{initial_length - final_length} entries in the dataset have been dropped due to them having duplicate UPRNs (highest risk is kept).")
-print(f"There are {len(df)} dwellings in the final output, of which {num_exeter} are from the Exeter list.")
+print(f"There are {len(df)} dwellings in the final output, of which {num_exeter} are on the Exeter list.")
 
 df["Final_Score"] = round(df["Final_Score"], 2)
 
